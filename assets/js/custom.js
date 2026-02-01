b5563ced82c86dd64242876ea47eac249221f058
Sujin N <sujinnelson2k@gmail.com>
2026-02-01 17:43:50 +0530

    lkjh

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,15 @@
 document.querySelector(".mc-footer__scroll-top").addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+$(function () {
+    $(".mc-news__tab").on("click", function () {
+        const tabId = $(this).data("tab");
+
+        $(".mc-news__tab").removeClass("mc-news__tab--active");
+        $(this).addClass("mc-news__tab--active");
+
+        $(".mc-news__panel").removeClass("mc-news__panel--active");
+        $("#" + tabId).addClass("mc-news__panel--active");
+    });
+});
